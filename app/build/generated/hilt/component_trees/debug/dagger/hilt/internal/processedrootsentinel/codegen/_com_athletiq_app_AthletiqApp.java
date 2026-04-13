18bc39a6ca6e50980a7f6b16f4b92a640bdfe52a
Žiga Mrevlje <ziga.mrevlje@gmail.com>
2026-04-13 08:52:06 +0200

    chore: remove build artifacts from Git tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_athletiq_app_AthletiqApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.athletiq.app.AthletiqApp"
-)
-public final class _com_athletiq_app_AthletiqApp {
-}
